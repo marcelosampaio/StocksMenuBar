7d670d6c03ba61959ca653ed8461e66b33432d5e
Marcelo Sampaio <marcelosampaiio1965@gmail.com>
2024-08-01 12:41:06 -0300

    Initial Commit

---
+++ StocksMenuBar/StocksMenuBarApp.swift
@@ -0,0 +1,17 @@
+//
+//  StocksMenuBarApp.swift
+//  StocksMenuBar
+//
+//  Created by Marcelo Sampaio on 01/08/24.
+//
+
+import SwiftUI
+
+@main
+struct StocksMenuBarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
